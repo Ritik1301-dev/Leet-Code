e0739b14043da09abf4b8d135d8654fd21c4b4c1
Ritik1301-dev <koseritik67@gmail.com>
2026-09-26 22:40:47 +0530

    Time: 1 ms (77.43%), Space: 42.5 MB (50.45%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        ArrayList<Integer>list = new ArrayList<>();
+        while(true){
+            int sum = 0;
+            while(n > 0){
+                int digit = n%10;
+                sum += digit*digit;
+                n /= 10;
+            }
+            if(sum == 1) return true;
+            if(list.contains(sum)) return false;
+            else list.add(sum);
+            n=sum;
+        }
+    }
+}
